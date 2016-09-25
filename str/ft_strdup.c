c76b195926736ef8efa8467f456a63b676e1d7c7
Bulliby <gwells@e3r7p21.42.fr>
2016-09-25 14:50:27 +0200

    feat(master): modif ft_strdup ft_Strjoin

--- str/ft_strdup.c
+++ str/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: gwells <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/03 17:52:54 by gwells            #+#    #+#             */
-/*   Updated: 2015/03/22 10:08:04 by gwells           ###   ########.fr       */
+/*   Updated: 2016/09/25 14:49:13 by gwells           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,7 @@ char		*ft_strdup(const char *str)
 	str_copy = NULL;
 	count = 0;
 	size = (int)ft_strlen(str);
-	str_copy = ft_memalloc_fail(size * sizeof(char) + 1);
-	if (str_copy == NULL)
-		return (NULL);
+	str_copy = ft_memalloc_fail(sizeof(char) *size + 1);
 	while (count != size)
 	{
 		str_copy[count] = str[count];
--- str/ft_strjoin.c
+++ str/ft_strjoin.c
@@ -6,12 +6,13 @@
 /*   By: gwells <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/07 15:14:09 by gwells            #+#    #+#             */
-/*   Updated: 2015/03/22 10:08:34 by gwells           ###   ########.fr       */
+/*   Updated: 2016/09/25 14:48:48 by gwells           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
+/*
 char				*null(char *s)
 {
 	unsigned int	size;
@@ -38,6 +39,24 @@ char				*ft_strjoin(char const *s1, char const *s2)
 		str = ft_memalloc_fail(size);
 		if (str == NULL)
 			return (NULL);
+		ft_strcpy(str, s1);
+		ft_strcat(str, s2);
+		return (str);
+	}
+	return (NULL);
+}
+*/
+
+char		*ft_strjoin(char const *s1, char const *s2)
+{
+	if (!s1 && s2)
+		return (ft_strdup(s2));
+	if (!s2 && s1)
+		return (ft_strdup(s1));
+	else if (s1 && s2)
+	{
+		size = ft_strlen(s1) + ft_strlen(s2) + 1;
+		str = ft_memalloc_fail(size);
 		ft_strcpy(str, s1);
 		ft_strcat(str, s2);
 		return (str);
