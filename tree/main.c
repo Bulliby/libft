c11354ad66f7e0b297f068916bf52661b535ffcd
Guillaume Wells <wellsguillaume@gmail.com>
2020-11-07 16:30:07 +0100

    tree

--- tree/main.c
+++ tree/main.c
@@ -1,4 +1,6 @@
-#include <libft.h>
+#include "libft.h"
+#include <stdio.h>
+
 
 int main()
 {
@@ -6,6 +8,8 @@ int main()
     t_tree *root;
     t_tree *left;
     t_tree *right;
+
+    int tab[1000] = {0};
     
     root = (t_tree*)ft_memalloc_fail(sizeof(*root));
     left = (t_tree*)ft_memalloc_fail(sizeof(*left));
@@ -17,4 +21,27 @@ int main()
     root->value = ft_strncpy(root->value, "undeuxtroisquate", 250);
 
     return 0;
+}
+
+void insertNode(int value, t_tree *root)
+{
+    t_tree *new_node;
+
+    new_node = (t_tree*)ft_memalloc_fail(sizeof(*new_node));
+    new_node->left = NULL;
+    new_node->right = NULL;
+
+    if (value < root->value) {
+        if (root->left == NULL) {
+            root->left = new_node;
+        } else {
+            insertNode(value, root->left);
+        }
+    } else if (value > root->value) {
+        if (root->right == NULL) {
+            root->right = new_node;
+        } else {
+            insertNode(value, root->right);
+        }
+    }
 }
